ddab9d52a5f7b8ea0304af5e10bc6ecb61dbcd8a
Angel <angelSistemas21@gmail.com>
2022-07-13 19:11:47 -0500

    refactor: Quitando comentarios innecesarios

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -1,7 +1,6 @@
 const Reader = require("../../../lib/utils/Reader");
 
 describe("Test for Reader", () => {
-    // ! nota: La clase 'Reader' esta al mismo nivel que el archivo 'explorer.json' por eso solo se pone entre comillas
     const explorers = Reader.readJsonFile("explorers.json");
 
     test("1. Obtener el tamaño de la lista de explorers", () => {
