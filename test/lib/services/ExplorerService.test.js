df3af60558762c47ebaa5c99fe676c2607b682e8
Angel <angelSistemas21@gmail.com>
2022-07-13 20:04:38 -0500

    test: Se actualizo el resultado esperado del test 1 - Se agrego la lista de los explorers

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -7,7 +7,127 @@ describe("Unit test for ExplorerService class", () => {
     test("1) Filter explorer for mission node", () => {
         const explorerFilter = ExplorerService.filterByMission (explorers, "node");
         
-        expect (explorerFilter.length).toBe(10);
+        expect (explorerFilter).toEqual([
+            {
+                "name": "Woopa1",
+                "githubUsername": "ajolonauta1",
+                "score": 1,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa2",
+                "githubUsername": "ajolonauta2",
+                "score": 2,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "groovy",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa3",
+                "githubUsername": "ajolonauta3",
+                "score": 3,
+                "mission": "node",
+                "stacks": [
+                    "elixir",
+                    "groovy",
+                    "reasonML"
+                ]
+            },
+            {
+                "name": "Woopa4",
+                "githubUsername": "ajolonauta4",
+                "mission": "node",
+                "score": 4,
+                "stacks": [
+                    "javascript"
+                ]
+            },
+            {
+                "name": "Woopa5",
+                "githubUsername": "ajolonauta5",
+                "score": 5,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa11",
+                "githubUsername": "ajolonauta11",
+                "score": 11,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa12",
+                "githubUsername": "ajolonauta12",
+                "score": 12,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa13",
+                "githubUsername": "ajolonauta13",
+                "score": 13,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa14",
+                "githubUsername": "ajolonauta14",
+                "score": 14,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa15",
+                "githubUsername": "ajolonauta15",
+                "score": 15,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+        
+            }
+        ]);
     });
 
     test("2) Get amount of explorers by mission node", () => {
