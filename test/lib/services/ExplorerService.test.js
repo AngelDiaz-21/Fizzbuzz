582a37993df91c0aaf1556e2a20dc490c848d012
Angel <angelSistemas21@gmail.com>
2022-07-13 19:20:03 -0500

    refactor: Quitando comentarios innecesarios

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -2,7 +2,6 @@ const Reader = require ("../../../lib/utils/Reader");
 const ExplorerService = require("../../../lib/services/ExplorerService");
 
 describe("Unit test for ExplorerService class", () => {
-    // La constante se pone de forma general para que pueda ser utilizado por todos los test
     const explorers = Reader.readJsonFile("explorers.json");
     
     test("1) Filter explorer for mission node", () => {
@@ -14,15 +13,10 @@ describe("Unit test for ExplorerService class", () => {
     test("2) Get amount of explorers by mission node", () => {
         const getExplorerByMission = ExplorerService.getAmountOfExplorersByMission (explorers, "node");
         
-        // 1. Con esta opción, en el método original al return concatenamos "length" para obtener el número de explorers en node
         expect(getExplorerByMission).toBe(10);
-
-        // 2. Con esta opción, en el método original en la parte del return se regresaria solo la constante, entonces nos regresaría una lista de objetos y para que la prueba sea correcta en los puntos suspensivos se tendría que poner la misma lista de objetos
-        // expect(getExplorerByMission).toEqual([{...}]);
     });
 
     test("3) Get explorers usernames by mission node", () => {
-    
         const explorerUsernameByMission = ExplorerService.getExplorersUsernamesByMission (explorers, "node");
 
         expect(explorerUsernameByMission).toEqual([
